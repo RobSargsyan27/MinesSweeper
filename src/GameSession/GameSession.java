a7d85647507e3f48ad8fcd9392cf0af0ed3c2a40
matas0 <63170914+matas0@users.noreply.github.com>
2023-11-29 16:44:26 +0100

    Create GameSession.java

---
+++ src/GameSession/GameSession.java
@@ -0,0 +1,39 @@
+package GameSession;
+
+import java.util.Date;
+
+public class GameSession {
+
+    private String[][] openBoard;
+    private String time;
+    private String gameDifficulty;
+    private String userName;
+    private Date date;
+    private int moves;
+
+    public void save() {
+    }
+
+    public void end() {
+    }
+
+
+    public void play() {
+
+        boolean gameRunning = true;
+
+        while (gameRunning) {
+            save();
+
+         /*    if(input.equals("q")){
+                            end();
+
+        }*/
+
+        }
+
+        end();
+
+    }
+
+}
